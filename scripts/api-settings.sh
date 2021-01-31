d9b508590575e19ac9f3c5089442cff1952191f4
Dominik <dominik@my-account.net>
2021-01-31 23:42:55 +0100

    fix substitution

--- scripts/api-settings.sh
+++ scripts/api-settings.sh
@@ -3,34 +3,39 @@
 ############################################################
 # Domain settings
 
-option() {
+jsonOption() {
   key="$1"
   cat $CONFIG_FILE | jq -r ".$key"
 }
 
+value() {
+  env | egrep "^$1=" | cut -d= -f2
+}
+
 env_print() {
-  echo $1: $$1 > /dev/stderr
+  echo $1: $(value $1) > /dev/stderr
 }
 
 env_print_secret() {
-  if [ -z $$1 ]; then
+  if [ -z "$(value $1)" ]; then
     echo $1: > /dev/stderr
   else
     echo $1: 'xxxxxxxxxx' > /dev/stderr
   fi
 }
 
-DOMAIN=$(option domain)
+DOMAIN=`jsonOption domain`
 EMAIL=letsencrypt@${DOMAIN}
 ############################################################
 
 ############################################################
 # GoDaddy API Credentials
-GODADDY_API_KEY=$(option apiKey)
-GODADDY_API_SECRET=$(option apiSecret)
-GODADDY_URL=$(option apiUrl)
+GODADDY_API_KEY=`jsonOption apiKey`
+GODADDY_API_SECRET=`jsonOption apiSecret`
+GODADDY_URL=`jsonOption apiUrl`
 ############################################################
 
+env_print CONFIG_FILE
 env_print DOMAIN
 env_print GODADDY_API_KEY
 env_print_secret GODADDY_API_SECRET
