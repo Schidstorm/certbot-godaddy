71e3e893f60cf3d3cebee753baf44a828b659fef
Dominik <dominik@my-account.net>
2021-01-31 23:07:55 +0100

    add configfile path from arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-$command="$1"
+command="$1"
+export CONFIG_FILE=${CONFIG_FILE:-"$2"}
 
 if [ "$command" == "request" ]; then
   certbot-godaddy-request.sh
@@ -8,7 +9,7 @@ elif [ "$command" == "renew" ]; then
   certbot-godaddy-renew.sh
 else
   cat << _EOF
-usage: $0 request|renew
+usage: $0 request|renew [config-file.json]
 _EOF
 exit 1
 fi
