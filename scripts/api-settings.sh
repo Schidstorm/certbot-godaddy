68db05334bd2e189ad319b4b79cb530a3d399ea9
Dominik <dominik@my-account.net>
2021-01-31 23:22:54 +0100

    add debug env print

--- scripts/api-settings.sh
+++ scripts/api-settings.sh
@@ -8,6 +8,10 @@ option() {
   cat $CONFIG_FILE | jq -r ".$key"
 }
 
+env_print() {
+  echo ${$1} > /dev/stderr
+}
+
 DOMAIN=$(option domain)
 EMAIL=letsencrypt@${DOMAIN}
 ############################################################
@@ -18,3 +22,8 @@ GODADDY_API_KEY=$(option apiKey)
 GODADDY_API_SECRET=$(option apiSecret)
 GODADDY_URL=$(option apiUrl)
 ############################################################
+
+env_print DOMAIN
+env_print GODADDY_API_KEY
+env_print GODADDY_API_SECRET
+env_print GODADDY_URL
